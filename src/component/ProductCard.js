bd8c19d5be69beb0740667c65d646c43f3b5f8ca
Wonwoo Park <gn030108@gmail.com>
2024-08-19 22:24:16 +0900

    미디어 쿼리 수정

--- src/component/ProductCard.js
+++ src/component/ProductCard.js
@@ -1,23 +1,24 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
-const ProductCard = ({item}) => {
+const ProductCard = ({ item }) => {
+  const navigate = useNavigate();
 
-  const navigate=useNavigate()
-
-  const goProductDetail = ()=>{
-    navigate(`/product/${item.id}`)
-  }
+  const goProductDetail = () => {
+    navigate(`/product/${item.id}`);
+  };
 
   return (
-    <div className='card_Box' onClick={goProductDetail}>
-      <img src={item?.img} alt='img'/>
-      <div>{item?.choice === true? "Conscious choice" : ""}</div>
-      <div>{item?.title}</div>
-      <div>{item?.price}</div>
-      <div>{item?.new === true? "신제품" : ''}</div>
+    <div className="card_Box" onClick={goProductDetail}>
+      <div className="card_center">
+        <img src={item?.img} alt="img" />
+        <div>{item?.choice === true ? "Conscious choice" : ""}</div>
+        <div>{item?.title}</div>
+        <div>{item?.price}</div>
+        <div>{item?.new === true ? "신제품" : ""}</div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProductCard
+export default ProductCard;
